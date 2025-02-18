c9b3a2f62704e06cb5a595a6ba16ff1bb9be53f2
mankeipo <pomankei66666@gmail.com>
2025-02-18 18:48:32 -0500

    fixed some minor mistakes

--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -13,7 +13,7 @@ class Project extends React.Component {
             <div class="cards">
                 <Card name="Jeopardy" words="&emsp;&emsp;A full-stack web application for a real-time multiplayer Jeopardy game using React, JavaScript, and Express.js, implementing socket connections for live gameplay, a RESTful API for data management" type="Fullstack Application" link="https://github.com/csci-499-fa24/Team2/" img={jeopardy}></Card>
                 <Card name="AlumLink" words="&emsp;&emsp;A full-stack open source alumni directory solution designed for CUNY Law School graduates in a team of 4 and built with React, Node.js, Express.js, and PostgreSQL." type="Fullstack Application" link="https://github.com/CUNYTechPrep/AlumLink" img={alumlink}></Card>
-                <Card name="Stray-Finder" words="&emsp;&emsp;A full-stack web application for locating and tracking stray animals using React, JavaScript, and Express.js, integrating Google Maps API for stray coordinates, a RESTful API, AWS S3 for image storage, and Nodemailer for subscriber notifications" type="Frontend Application" link="https://github.com/csci-499-fa24/Stray-Finder" img={stray_finder}></Card>
+                <Card name="Stray-Finder" words="&emsp;&emsp;A full-stack web application for locating and tracking stray animals using React, JavaScript, and Express.js, integrating Google Maps API for stray coordinates, a RESTful API, AWS S3 for image storage, and Nodemailer for subscriber notifications" type="Fullstack Application" link="https://github.com/csci-499-fa24/Stray-Finder" img={stray_finder}></Card>
             </div>
             
         </div>
